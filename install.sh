36dc58182ee184f9ec2eb012e58462a3f70f3102
leespeng <120107173+leespeng@users.noreply.github.com>
2026-05-29 09:18:25 +0800

    install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p /opt/s-ui && cd /opt/s-ui
-wget -O s-ui-1.4.2_full.tar.gz "https://c.wss.ink/f/k0i79dzabxg"
-tar -zxvf s-ui-1.4.2_full.tar.gz
-bash install.sh
-systemctl start s-ui
-systemctl enable s-ui
-echo "安装完成！访问地址：http://$(hostname -I | awk '{print $1}'):2095/app/"
